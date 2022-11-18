b929458b0e1fd788a5f8fa7511f25b8f39bc6dff
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2022-11-19 02:31:35 +0900

    chore: use include guard
    Signed-off-by: Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>

--- include/speak_ros/speak_ros.hpp
+++ include/speak_ros/speak_ros.hpp
@@ -12,6 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#ifndef SPEAK_ROS__SPEAK_ROS_HPP_
+#define SPEAK_ROS__SPEAK_ROS_HPP_
+
 #include <memory>
 #include <string>
 
@@ -28,3 +31,5 @@ private:
 };
 
 }  // namespace speak_ros
+
+#endif  // SPEAK_ROS__SPEAK_ROS_HPP_
